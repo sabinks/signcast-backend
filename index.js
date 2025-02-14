0caf6091a713bdcd91d940fd75737104db65daaf
sabinks <sabin.kr.stha@gmail.com>
2025-02-14 09:18:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+
+const socket = require('socket.io')(server, {
+    cors: { origin: "*" },
+    rejectUnauthorized: false,
+    transports: ["websocket", "polling"],
+    pingTimeout: 60000
+});
+const io = socket.of('/socket.io');
+
+app.use(cors());
+app.use(express.json());
+
+const contentFile = "./content.json";
+
+// WebSocket connections
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    // Send stored content to new clients
+    if (fs.existsSync(contentFile)) {
+        const content = JSON.parse(fs.readFileSync(contentFile, "utf-8"));
+        socket.emit("update-content", content);
+    }
+
+    // Sync content from dashboard to Electron clients
+    socket.on("sync-content", (data) => {
+        fs.writeFileSync(contentFile, JSON.stringify(data, null, 2));
+        console.log('sync-content');
+        io.emit("update-content", data);
+    });
+    socket.on("request-content", (data) => {
+        const content = JSON.parse(fs.readFileSync(contentFile, "utf-8"));
+        io.emit("update-content", content);
+    });
+
+    socket.on("disconnect", () => console.log("Client disconnected:", socket.id));
+});
+
+app.get("/content", (req, res) => {
+    if (fs.existsSync(contentFile)) {
+        return res.json(JSON.parse(fs.readFileSync(contentFile, "utf-8")));
+    }
+    res.json({});
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
