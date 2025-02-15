9303ba083239ed2b43960ae03dc9a184529fd433
sabinks <sabin.kr.stha@gmail.com>
2025-02-15 14:27:06 -0500

    changes pushed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const http = require('http');
-const fs = require('fs');
-const cors = require('cors');
+import express from 'express';
+import http from 'http'
+import fs from 'fs';
+import cors from 'cors';
+import 'dotenv/config';
+import { Server } from 'socket.io';
+import connectDB from './config/db.js'
+import { Screen } from './model/screen.js';
+
 const app = express();
 const server = http.createServer(app);
-require('dotenv').config()
 
-const socket = require('socket.io')(server, {
+const socket = new Server(server, {
     cors: { origin: "*" },
     rejectUnauthorized: false,
     transports: ["websocket", "polling"],
@@ -14,43 +18,75 @@ const socket = require('socket.io')(server, {
 });
 
 const io = socket.of('/socket.io');
-
+connectDB()
 app.use(cors());
 app.use(express.json());
 
-const contentFile = "./content.json";
+const connectedDevices = []
 
-// WebSocket connections
 io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
-
-    // Send stored content to new clients
-    if (fs.existsSync(contentFile)) {
-        const content = JSON.parse(fs.readFileSync(contentFile, "utf-8"));
-        socket.emit("update-content", content);
+    const devicePresent = connectedDevices.includes(socket.id)
+    if (!devicePresent) {
+        connectedDevices.push({ id: socket.id, status: 'online', screenId: null })
     }
 
-    // Sync content from dashboard to Electron clients
-    socket.on("sync-content", (data) => {
-        fs.writeFileSync(contentFile, JSON.stringify(data, null, 2));
-        console.log('sync-content');
-        io.emit("update-content", data);
+    socket.on("get-all-screens", async () => {
+        const screens = await Screen.find({})
+        io.emit("list-screens", screens);
     });
 
-    socket.on("request-content", (data) => {
-        const content = JSON.parse(fs.readFileSync(contentFile, "utf-8"));
-        io.emit("update-content", content);
+    socket.on("add-screen", async (data) => {
+        const screen = Screen({
+            screenId: data.screenId,
+            name: data.name,
+            content: data.content
+        })
+        await screen.save();
+        const screens = await Screen.find({})
+        io.emit("list-screens", screens);
     });
 
-    socket.on("disconnect", () => console.log("Client disconnected:", socket.id));
+    socket.on("update-screen", async (data) => {
+        const screen = await Screen.findOne({ _id: data.id });
+        screen.screenId = data.payload.screenId;
+        screen.name = data.payload.name;
+        screen.content = data.payload.content;
+        await screen.save();
+        const screens = await Screen.find({})
+        io.emit("list-screens", screens);
+    });
 
-});
+    socket.on("delete-screen", async (payload) => {
+        const { screenId } = payload
+        await Screen.deleteOne({ screenId });
+        const screens = await Screen.find({})
+        io.emit("list-screens", screens);
+    });
 
-app.get("/content", (req, res) => {
-    if (fs.existsSync(contentFile)) {
-        return res.json(JSON.parse(fs.readFileSync(contentFile, "utf-8")));
-    }
-    res.json({});
+    socket.on("request-content", async (payload) => {
+        const { screenId } = payload
+        const index = connectedDevices.findIndex(device => device.id === socket.id)
+        connectedDevices[index].sceeenId = screenId
+        const screen = await Screen.findOne({ screenId });
+        // const content = JSON.parse(fs.readFileSync(contentFile, "utf-8"));
+        io.emit("update-content", screen);
+    });
+    socket.on("sync-screen-with-devices", async (payload) => {
+        const { screenId } = payload
+        console.log(connectedDevices);
+        const screen = await Screen.findOne({ screenId });
+        let groupedScreen = connectedDevices.filter(device => device.screenId = screenId)
+        groupedScreen.forEach(device => {
+            io.to(device.id).emit("update-content", screen);
+        })
+    })
+
+    socket.on("disconnect", () => {
+        const index = connectedDevices.findIndex(device => device.id === socket.id)
+        connectedDevices[index].status = 'offline'
+        console.log("Client disconnected:", socket.id)
+    });
 });
 
 const port = process.env.PORT || 5000
---
+++ model/screen.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+import Joi from 'joi'
+
+const screenSchema = new Schema({
+    screenId: { type: String, required: true },
+    name: { type: String, required: true },
+    content: { type: Object, required: true },
+}, {
+    timestamps: true
+})
+
+function validateScreen(data) {
+    const schema = Joi.object({
+        screenId: Joi.string().required(),
+        name: Joi.string().required(),
+        content: Joi.string().required(),
+    })
+    return schema.validate(data, { abortEarly: false })
+}
+
+const Screen = model('Screen', screenSchema)
+export { Screen, screenSchema, validateScreen }
